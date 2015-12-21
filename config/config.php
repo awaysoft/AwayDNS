682d3fb5a146b54c7c3b45ff863ffeecd6518d59
Tom <tom@awaysoft.com>
2015-12-22 00:43:50 +0800

    replace tab to space

--- config/config.php
+++ config/config.php
@@ -1,22 +1,22 @@
 <?php
-	return [
-        'server' => [
-            'ip' => '0.0.0.0',
-            'port' => 53,
-            'work_num' => 1
+return [
+    'server' => [
+        'ip' => '0.0.0.0',
+        'port' => 53,
+        'work_num' => 1
+    ],
+    'providers' => [
+        'hosts' => [
+            // 'path' => '/etc/hosts', /* Load system default file when no setting */
+            'default_ttl' => 300,
+            'refresh_time' => 60,
         ],
-		'providers' => [
-            'hosts' => [
-                // 'path' => '/etc/hosts', /* Load system default file when no setting */
-                'default_ttl' => 300,
-                'refresh_time' => 60,
-            ],
-			'json' => [
-				'data' => 'dns_record.json',
-                'default_ttl' => 300,
-			],
-			//'recursive' => [
-				
-			//]
-		]
-	];
+        'json' => [
+            'data' => 'dns_record.json',
+            'default_ttl' => 300,
+        ],
+        //'recursive' => [
+            
+        //]
+    ]
+];
